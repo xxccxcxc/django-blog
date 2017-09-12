4f33a72e44eec3dbecb549e34387271f232ac1f6
xxccxcxc <784066071@qq.com>
2017-09-12 22:19:58 +0800

    add class Comment to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from blog.models import Category, Tag, Post
+from comments.models import Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
@@ -8,3 +9,4 @@ class PostAdmin(admin.ModelAdmin):
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
