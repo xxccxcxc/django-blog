aea0a12ffd8fdae01b53b9ed60ca199f53cf1df1
xxccxcxc <784066071@qq.com>
2017-09-16 16:22:56 +0800

    use listview

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,44 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post, Category
 from comments.forms import CommentForm
+from django.views.generic import ListView
 import markdown
 import pygments
 
 
+'''
 def index(request):
     post_list = Post.objects.all()
     return render(request, 'blog/index.html', context={'post_list': post_list})
+'''
 
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+'''
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+'''
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+'''
+def archives(request, year, month):
+    post_list = Post.objects.all().filter(created_time__year=year, created_time__month=month)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+'''
+
+class ArchivesView(IndexView):
+    def get_queryset(self):
+        return super(ArchivesView, self).get_queryset().filter(created_time__year=self.kwargs.get('year'),
+                                                               created_time__month=self.kwargs.get('month'))
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -27,14 +57,3 @@ def detail(request, pk):
         'comment_list': comment_list,
     }
     return render(request, 'blog/detail.html', context=context)
-
-
-def archives(request, year, month):
-    post_list = Post.objects.all().filter(created_time__year=year, created_time__month=month).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
-
-
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate)
-    return render(request, 'blog/index.html', context={'post_list': post_list})
