5072be41118e4a3cd6f89e66448aab65d4ee2e26
xxccxcxc <784066071@qq.com>
2017-09-19 20:22:25 +0800

    use class DetailView

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post, Category
 from comments.forms import CommentForm
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 import markdown
 import pygments
 
@@ -41,7 +41,7 @@ class ArchivesView(IndexView):
         return super(ArchivesView, self).get_queryset().filter(created_time__year=self.kwargs.get('year'),
                                                                created_time__month=self.kwargs.get('month'))
 
-def detail(request, pk):
+'''def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.increase_views()
     post.body = markdown.markdown(post.body, extensions=[
@@ -56,4 +56,35 @@ def detail(request, pk):
         'form': form,
         'comment_list': comment_list,
     }
-    return render(request, 'blog/detail.html', context=context)
+    return render(request, 'blog/detail.html', context=context)'''
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+
+    def get_object(self):
+        post = super(PostDetailView, self).get_object()
+        post.body = markdown.markdown(post.body, extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.toc',
+        ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list,
+        })
+        return context
+
