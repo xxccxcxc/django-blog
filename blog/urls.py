01f5a0a3ab3305e6afd49988080ef08b38ce3d5f
xxccxcxc <784066071@qq.com>
2017-09-22 21:53:36 +0800

    加入关于标签的若干功能

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='detail'),
     url(r'^archives/(?P<year>\d{4})/(?P<month>\d{1,2})/$', views.ArchivesView.as_view(), name='archives'),
     url(r'^category/(?P<pk>\d+)/$', views.CategoryView.as_view(), name='category'),
+    url(r'^tag/(?P<pk>\d+)/$', views.TagView.as_view(), name='tag'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post, Category
+from .models import Post, Category, Tag
 from comments.forms import CommentForm
 from django.views.generic import ListView, DetailView
 import markdown
 import pygments
-
-
-'''
-def index(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/index.html', context={'post_list': post_list})
-'''
 
 class IndexView(ListView):
     model = Post
@@ -56,45 +49,23 @@ class IndexView(ListView):
         }
         return data
 
-'''
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate)
-    return render(request, 'blog/index.html', context={'post_list': post_list})
-'''
 
 class CategoryView(ListView):
     def get_queryset(self):
         cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
         return super().get_queryset().filter(category=cate)
 
-'''
-def archives(request, year, month):
-    post_list = Post.objects.all().filter(created_time__year=year, created_time__month=month)
-    return render(request, 'blog/index.html', context={'post_list': post_list})
-'''
 
 class ArchivesView(IndexView):
     def get_queryset(self):
         return super().get_queryset().filter(created_time__year=self.kwargs.get('year'),
-                                                               created_time__month=self.kwargs.get('month'))
+                                             created_time__month=self.kwargs.get('month'))
 
-'''def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.increase_views()
-    post.body = markdown.markdown(post.body, extensions=[
-                                     'markdown.extensions.extra',
-                                     'markdown.extensions.codehilite',
-                                     'markdown.extensions.toc',
-                                 ])
-    form = CommentForm()
-    comment_list = post.comment_set.all()
-    context = {
-        'post': post,
-        'form': form,
-        'comment_list': comment_list,
-    }
-    return render(request, 'blog/detail.html', context=context)'''
+class TagView(IndexView):
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super().get_queryset().filter(tags=tag)
+
 
 class PostDetailView(DetailView):
     model = Post
