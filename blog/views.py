31a061ecd9ea714a58bd1d8116d5712f059854c3
xxccxcxc <784066071@qq.com>
2017-09-27 19:50:33 +0800

    fix a bug

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ class IndexView(ListView):
     paginate_by = 10
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super(ListView, self).get_context_data(**kwargs)
         paginator = context.get('paginator')
         page = context.get('page_obj')
         is_paginated = context.get('is_paginated')
@@ -55,18 +55,18 @@ class IndexView(ListView):
 class CategoryView(ListView):
     def get_queryset(self):
         cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
-        return super().get_queryset().filter(category=cate)
+        return super(ListView, self).get_queryset().filter(category=cate)
 
 
 class ArchivesView(IndexView):
     def get_queryset(self):
-        return super().get_queryset().filter(created_time__year=self.kwargs.get('year'),
+        return super(IndexView, self).get_queryset().filter(created_time__year=self.kwargs.get('year'),
                                              created_time__month=self.kwargs.get('month'))
 
 class TagView(IndexView):
     def get_queryset(self):
         tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
-        return super().get_queryset().filter(tags=tag)
+        return super(IndexView, self).get_queryset().filter(tags=tag)
 
 
 class PostDetailView(DetailView):
@@ -75,12 +75,12 @@ class PostDetailView(DetailView):
     context_object_name = 'post'
 
     def get(self, request, *args, **kwargs):
-        response = super().get(request, *args, **kwargs)
+        response = super(DetailView, self).get(request, *args, **kwargs)
         self.object.increase_views()
         return response
 
     def get_object(self):
-        post = super().get_object()
+        post = super(DetailView, self).get_object()
         md = markdown.Markdown(extensions=[
             'markdown.extensions.extra',
             'markdown.extensions.codehilite',
@@ -91,7 +91,7 @@ class PostDetailView(DetailView):
         return post
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super(DetailView, self).get_context_data(**kwargs)
         form = CommentForm()
         comment_list = self.object.comment_set.all()
         context.update({
