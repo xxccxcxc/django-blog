5ab4809b9a1d483946b3bbc84fdf3bf1ce969dde
xxccxcxc <784066071@qq.com>
2018-02-27 20:47:33 +0800

    修改标签排版

--- blog/models.py
+++ blog/models.py
@@ -19,6 +19,9 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ['-name']
+
 
 class Post(models.Model):
     title = models.CharField(max_length=70)
