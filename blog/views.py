219512f2b5c806c5376ead17a6273b01c73d9825
xxccxcxc <784066071@qq.com>
2018-03-05 23:00:31 +0800

    修改一言查询范围

--- blog/views.py
+++ blog/views.py
@@ -59,7 +59,7 @@ class IndexView(ListView):
         return data
 
     def get_hitokoto(self):
-        response = requests.get('https://sslapi.hitokoto.cn/?c=a')
+        response = requests.get('https://sslapi.hitokoto.cn/')
         dict = json.loads(response.text)
         hitokoto = {
             'hitokoto': dict['hitokoto'],
@@ -174,7 +174,7 @@ class PostDetailView(DetailView):
 
 
     def get_hitokoto(self):
-        response = requests.get('https://sslapi.hitokoto.cn/?c=a')
+        response = requests.get('https://sslapi.hitokoto.cn/')
         dict = json.loads(response.text)
         hitokoto = {
             'hitokoto': dict['hitokoto'],
