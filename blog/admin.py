a8eb5fc78cf955dba53fe90fcfdef9c4663160eb
xxccxcxc <784066071@qq.com>
2018-03-08 21:10:01 +0800

    修改几个字段默认值，修改日期显示格式

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,11 @@ from comments.models import Comment
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
 
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'author':
+            kwargs['initial'] = request.user.id
+        return super(PostAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 admin.site.register(Category)
 admin.site.register(Tag)
