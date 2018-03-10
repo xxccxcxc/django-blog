4759bcbfff70640f2bf681ab4d8b67e241941c94
xxccxcxc <784066071@qq.com>
2018-03-10 13:15:14 +0800

    修复侧边栏定位bug

--- blog/static/blog/js/scrollFix.js
+++ blog/static/blog/js/scrollFix.js
@@ -9,22 +9,23 @@ $.fn.scrollFix = function (opt) {
         $main = $(settings.main),
 		$top = $(settings.top),
         mainHeight,
-		bottomMargin = 50;
+		bottomDis = 50;
     $window.scroll(function () {
         windowHeight = $window.height();
-        scrollTop = $window.scrollTop() - $top.height() - bottomMargin;
-        mainHeight = $main.height();
+        scrollTop = $window.scrollTop();
+        mainHeight = $main.outerHeight(true);
+		topHeight = $top.outerHeight(true);
         $this.each(function (idx, val) {
-            thisHeight = $(this).height();
+            thisHeight = $(this).outerHeight(true);
             // 当主体区高度小于侧边栏时不做处理
             if (mainHeight < thisHeight) {
                 return false;
             }
             // 当主体区高度大于侧边栏，且下滚高度+窗口高度大于侧边栏高度时，固定侧边栏
-            if (scrollTop + windowHeight > thisHeight) {
+            if (scrollTop + windowHeight > topHeight + thisHeight + bottomDis) {
                 $(this).css({
                     position: 'fixed',
-                    bottom: bottomMargin
+                    bottom: bottomDis
                 });
             } else {
                 $(this).css({
